b1e5f9c4a4aa37dde0b88102fca7764e98865b95
Jamie <deluxe@jaiii.me>
2025-07-17 09:30:35 +0100

    feat: implements average response time by status code

--- index.js
+++ index.js
@@ -15,53 +15,46 @@ const requests = file.split("\n");
 const ips = new Map();
 const routes = new Map();
 const exitCodes = new Map();
+const exitCodesResponseTimesSum = new Map();
 let startDate, endDate;
-let processed = 0;
 
 for (let i = 0; i < requests.length - 1; i++) {
 	const request = requests[i];
-    const match = request.match(
+	const match = request.match(
 		/^(\d{1,3}(?:\.\d{1,3}){3}) - ([A-Z]{2}) - \[([^\]]+)] "(\w+) ([^"]+) HTTP\/[\d.]+" (\d{3}) (\d+) "([^"]*)" "([^"]*)" (\d+)$/
 	);
-    if (!match) {
-        console.log(i, request);
-        continue;
-    }
-    const [
-        ip,
-        countryCode,
-        dateString,
-        method,
-        route,
-        statusCode,
-        responseSize,
-        referrer,
-        userAgent,
-        responseTime
-    ] = match.slice(1);
-    processed++;
+	// This log line does not conform to the log file standard
+	if (!match) continue;
+	const [
+		ip,
+		countryCode,
+		dateString,
+		method,
+		route,
+		statusCode,
+		responseSize,
+		referrer,
+		userAgent,
+		responseTime,
+	] = match.slice(1);
 
-    ips.set(ip, (ips.get(ip) ?? 0) + 1);
-    routes.set(route, (routes.get(route) ?? 0) + 1);
-    exitCodes.set(statusCode, (exitCodes.get(statusCode) ?? 0) + 1);
+	ips.set(ip, (ips.get(ip) ?? 0) + 1);
+	routes.set(route, (routes.get(route) ?? 0) + 1);
+	exitCodes.set(statusCode, (exitCodes.get(statusCode) ?? 0) + 1);
+	exitCodesResponseTimesSum.set(
+		statusCode,
+		(exitCodesResponseTimesSum.get(statusCode) ?? 0) + +responseTime
+	);
 
 	if (i == 0) startDate = dateString;
 	else if (i == requests.length - 2) endDate = dateString;
 }
 
-console.log("processed: " + processed);
-
-console.log(startDate, endDate);
-
-const top10Ips = utils.getTop10ActivityIPs([...ips.entries()]);
-const top10Routes = utils.getTop10ActivityIPs([...routes.entries()]);
-console.log("ips: " + ips.size);
-console.log(top10Ips);
-console.log("routes: " + routes.size);
-console.log(top10Routes);
-
-console.log("status codes: " + exitCodes.size);
-console.log(exitCodes);
+const top10Ips = utils.getTop10ByKVP([...ips.entries()]);
+const top10Routes = utils.getTop10ByKVP([...routes.entries()]);
+const averageResponseSpeedByStatusCode = [
+	...exitCodesResponseTimesSum.entries(),
+].map(([statusCode, val]) => [statusCode, val / exitCodes.get(statusCode)]);
 
 const activityByHighestActivityIps = top10Ips.reduce(
 	(prev, [_ip, activity]) => prev + activity,
@@ -79,16 +72,15 @@ console.log(
 		`Proportion of Requests made by Top 10 IPs: ${
 			(activityByHighestActivityIps * 100) / requests.length
 		}%`,
+		`\nAverage Response Time By Status Code`,
+		...averageResponseSpeedByStatusCode.map(
+			([code, time]) => `Status ${code} : ${time.toFixed(5)}`
+		),
 	].join("\n")
 );
 
-function sortMap(map) {
-    return [...map.entries()].sort((a, b) => b[1] - a[1]);
-}
+// const ipObj = Object.fromEntries(sortMap(ips));
+// const routesObj = Object.fromEntries(sortMap(routes));
 
-const ipObj = Object.fromEntries(sortMap(ips));
-const routesObj = Object.fromEntries(sortMap(routes));
-
-fs.writeFileSync('ips.json', JSON.stringify(ipObj, null, 2, 'utf-8'));
-fs.writeFileSync('routes.json', JSON.stringify(routesObj, null, 2, 'utf-8'));
-
+// fs.writeFileSync("ips.json", JSON.stringify(ipObj, null, 2, "utf-8"));
+// fs.writeFileSync("routes.json", JSON.stringify(routesObj, null, 2, "utf-8"));
--- utils.js
+++ utils.js
@@ -1,14 +1,21 @@
 module.exports = {
 	/**
 	 *
-	 * @param {Array} ips An array of IP, activity array pairs (likely the result of a <Map>.entries call)
-	 * @returns {Array} An array of the top 10 ips (in the same key value pairs)
+	 * @param {Array} ips An array of IP, value array pairs (likely the result of a <Map>.entries call)
+	 * @returns {Array} An array of the top 10 ips (in the same key value pairs) sorted descending by the value associated with the ip
 	 */
-	getTop10ActivityIPs: (ips) => {
+	getTop10ByKVP: (ips) => {
 		const highestActivityIps = ips.sort(
 			([_ipA, activityA], [_ipB, activityB]) => activityB - activityA
 		);
 		highestActivityIps.length = Math.min(highestActivityIps.length, 10);
 		return highestActivityIps;
 	},
+
+	/**
+	 *
+	 * @param {Map} map The map to sort by the associated values
+	 * @returns An array, sorted descending by the value
+	 */
+	sortMap: (map) => [...map.entries()].sort(([_k1, v1], [_k2, v2]) => v2 - v1),
 };
